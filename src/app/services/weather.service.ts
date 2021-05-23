9065c5ba9545af3025dafe56124faa63123d564c
AndrewJBateman <gomezbateman@yahoo.com>
2021-05-23 15:03:18 +0200

    chore: access credential removed

--- src/app/services/weather.service.ts
+++ src/app/services/weather.service.ts
@@ -1,17 +1,17 @@
 import { Injectable } from "@angular/core";
 import { HttpClient } from "@angular/common/http";
 
-const apiKey = "45be76ae867d6487064c93552078b67f";
+const apiKey = "YOUR API KEY HERE";
 
 @Injectable({
-	providedIn: "root",
+  providedIn: "root",
 })
 export class WeatherService {
-	constructor(private httpClient: HttpClient) {}
+  constructor(private httpClient: HttpClient) {}
 
-	getWeatherFromApi(city: string) {
-		return this.httpClient.get(
-			`http://api.weatherstack.com/current?access_key=${apiKey}&query=${city}`
-		);
-	}
+  getWeatherFromApi(city: string) {
+    return this.httpClient.get(
+      `http://api.weatherstack.com/current?access_key=${apiKey}&query=${city}`
+    );
+  }
 }
