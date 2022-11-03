d57fe4c0b44b746111bbe9bfa76872c6cb123367
AndrewJBateman <gomezbateman@yahoo.com>
2022-11-03 16:27:31 +0100

    fix: use untyped form to remove partials error

--- src/app/list/list.page.ts
+++ src/app/list/list.page.ts
@@ -1,62 +1,62 @@
 import { Component, OnInit } from "@angular/core";
 import { WeatherService } from "../services/weather.service";
-import { FormGroup, FormControl, Validators } from "@angular/forms";
+import { FormControl, Validators, UntypedFormGroup } from "@angular/forms";
 import { Storage } from "@ionic/storage-angular";
 
 @Component({
-	selector: "app-list",
-	templateUrl: "list.page.html",
-	styleUrls: ["list.page.scss"],
+  selector: "app-list",
+  templateUrl: "list.page.html",
+  styleUrls: ["list.page.scss"],
 })
 export class ListPage implements OnInit {
-	constructor(
-		private weatherService: WeatherService,
-		private storage: Storage
-	) {}
+  constructor(
+    private weatherService: WeatherService,
+    private storage: Storage
+  ) {}
 
-	public weatherForm = new FormGroup({
-		city: new FormControl("", Validators.required),
-	});
-	public weather: Object;
-	public city: string;
+  public weatherForm = new UntypedFormGroup({
+    city: new FormControl("", Validators.required),
+  });
+  public weather: Object;
+  public city: string;
 
-	search(formData: FormData) {
-		console.log(formData);
-		this.storage.set("city", formData["city"]);
+  search(formData: FormData) {
+    console.log("formData: ", formData);
+    this.storage.set("city", formData["city"]);
 
-		this.weatherService
-			.getWeatherFromApi(formData["city"])
-			.subscribe((weather) => {
-				this.weather = weather;
-				console.log(weather);
-			});
-	}
+    this.weatherService
+      .getWeatherFromApi(formData["city"])
+      .subscribe((weather) => {
+        this.weather = weather;
+        console.log(weather);
+      });
+  }
 
-	getWeather() {
-		this.storage
-			.get("city")
-			.then((city) => {
-				if (city === null) {
-					this.weatherService
-						.getWeatherFromApi("madrid")
-						.subscribe((weather) => {
-							this.weather = weather;
-							console.log("weather from madrid, storage empty", weather);
-						});
-				} else {
-					this.weatherService.getWeatherFromApi(city).subscribe((weather) => {
-						this.weather = weather;
-						console.log(weather);
-					});
-				}
-			})
-			.catch((err) => {
-				console.log(err);
-			});
-	}
+  getWeather() {
+    this.storage
+      .get("city")
+      .then((city) => {
+        if (city === null) {
+          this.weatherService
+            .getWeatherFromApi("madrid")
+            .subscribe((weather) => {
+              this.weather = weather;
+              console.log("weather from madrid, storage empty", weather);
+            });
+        } else {
+          this.weatherService.getWeatherFromApi(city).subscribe((weather) => {
+            this.weather = weather;
+            console.log(weather);
+          });
+        }
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  }
 
-	async ngOnInit() {
+  async ngOnInit() {
     await this.storage.create();
-		this.getWeather();
-	}
+    this.getWeather();
+  }
 }
